b0406af7bbcd610824fe9b9186d104519e467b75
Lucas Marra <marralucas1@gmail.com>
2023-10-18 18:41:07 -0300

    sanitize cep

--- pages/index.vue
+++ pages/index.vue
@@ -3,46 +3,21 @@
     <v-container fill-height>
       <v-row>
         <v-col align="center" justify="center">
-          <v-sheet
-            min-height="20vh"
-            rounded="lg"
-            elevation="5"
-            class="content__sheet content_sheet-search"
-          >
+          <v-sheet min-height="20vh" rounded="lg" elevation="5" class="content__sheet content_sheet-search">
             <v-form v-model="valid">
               <v-container>
-                <v-text-field
-                  v-model="cep"
-                  v-mask.raw="'#####-###'"
-                  solo
-                  prepend-inner-icon="mdi-map-marker"
-                  :rules="cepRules"
-                  label="Informe o CEP"
-                  placeholder="00000-000"
-                  required
-                ></v-text-field>
+                <v-text-field v-model="cep" v-mask.raw="'#####-###'" solo prepend-inner-icon="mdi-map-marker"
+                  :rules="cepRules" label="Informe o CEP" placeholder="00000-000" required></v-text-field>
 
-                <v-btn
-                  :loading="loading"
-                  :disabled="!cep ||cep >= 3"
-                  class="content__sheet-button"
-                  elevation="2"
-                  type="submit"
-                  @click.prevent="fetchCep"
-                >
+                <v-btn :loading="loading" :disabled="!cep || cep >= 3" class="content__sheet-button" elevation="2"
+                  type="submit" @click.prevent="fetchCep">
                   <v-icon left>mdi-magnify</v-icon>Buscar CEP
                 </v-btn>
               </v-container>
             </v-form>
           </v-sheet>
 
-          <v-sheet
-            v-if="show"
-            min-height="20vh"
-            rounded="lg"
-            elevation="5"
-            class="content__sheet content__sheet-result"
-          >
+          <v-sheet v-if="show" min-height="20vh" rounded="lg" elevation="5" class="content__sheet content__sheet-result">
             <v-container class="content__container">
               <v-row v-if="error">
                 <v-col cols="12">{{ error }}</v-col>
@@ -75,10 +50,8 @@
                   </p>
                   <p>
                     <v-badge icon="mdi-open-in-new" color="blue-grey lighten-2">
-                      <a
-                        :href="`https://www.google.com.br/maps/search/${data.logradouro}`"
-                        target="_blank"
-                      >Google Maps</a>
+                      <a :href="`https://www.google.com.br/maps/search/${data.logradouro}`" target="_blank">Google
+                        Maps</a>
                     </v-badge>
                   </p>
                 </v-col>
@@ -112,8 +85,9 @@ export default {
       this.error = false
       this.show = false
       try {
+        const cepWithoutDash = this.cep.replace('-', '');
         const response = await this.$axios.$get(
-          `https://node-juntos-somos-mais.onrender.com/api/cep/${this.cep}`
+          `https://node-juntos-somos-mais.onrender.com/api/cep/${cepWithoutDash}`
         )
         this.data = response
       } catch (error) {
